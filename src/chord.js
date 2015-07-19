0e96b5c15bc67efd6ac92e23d4666d4d7efb7349
cholalo <violinist.ch@gmail.com>
2015-07-19 23:29:30 +0900

    ...

--- src/chord.js
+++ src/chord.js
@@ -1,9 +1,17 @@
 // JavaScript Document
 
-var Chord = function()
+var Chord = function( tonality )
 {
+    if( tonality ) this.set( tonality )
 }
 
-Chord.prototype.set = function()
+Chord.prototype.set = function( tonality )
+{
+    this.note = tonality.substr(0,1).toUpperCase() + tonality.substr(1, tonality.length-7).toLowerCase();
+    this.quality = tonality.substr(tonality.length - 5).toLowerCase();
+    this.tonality = this.note + ' ' + this.quality;
+}
+
+Chord.prototype.get = function()
 {
 }
--- src/harmony.js
+++ src/harmony.js
@@ -2,8 +2,20 @@
 
 var Harmony = function()
 {
+    if( tonality ) this.set( tonality )
 }
 
 Harmony.prototype.set = function()
 {
+    this.note = tonality.substr(0,1).toUpperCase() + tonality.substr(1, tonality.length-7).toLowerCase();
+    this.quality = tonality.substr(tonality.length - 5).toLowerCase();
+    this.tonality = this.note + ' ' + this.quality;
+}
+
+Harmony.prototype.get = function()
+{
+    var name = {
+        "major" : ['T', 'Sp', 'Dp', 'S', 'D', 'Tp'],
+        "minor" : ['t', '...']
+    }
 }
